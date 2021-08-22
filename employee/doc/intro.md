394ad532ac98a677efd06986e0520a9a91518463
Dusan Petrovic <dusanp93@live.com>
2021-08-22 19:58:56 +0200

    create lein project

---
+++ employee/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to employee
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ employee/test/employee/core_test.clj
@@ -0,0 +1,7 @@
+(ns employee.core-test
+  (:require [clojure.test :refer :all]
+            [employee.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

ca6192a4f7fddd37531a25a3c66968f3a17ee844
Dusan Petrovic <dusanp93@live.com>
2021-08-22 20:15:05 +0200

    add db queries

--- employee/src/employee/database.clj
+++ employee/src/employee/database.clj
@@ -10,9 +10,40 @@
    :password ""}) ;;add your password for local db
 
     
- (defn set-sector [sector]
-   (if (= sector "Marketing") 1
-   (if (= sector "Accounting") 2
-   (if (= sector "IT") 3
-   (if (= sector "Human Resources") 4
-   (if (= sector "Other") 5))))))
+ (defn set-department [department]
+   (if (= department "Marketing") 1
+   (if (= department "Accounting") 2
+   (if (= department "IT") 3
+   (if (= department "Human Resources") 4
+   (if (= department "Other") 5))))))
+
+
+(defn get-all-employees []
+  (into [] (sql/query connection ["select * from employees"])))
+
+(defn update-employee[id name department salary]
+  (sql/update! connection :employees {:id id :name name :category (set-department department) :salary salary} ["id = ?" id]))
+
+(defn get-employee [id]
+  (into [] (sql/query connection ["select * from employees where id = ?" id])))
+
+(defn find-by-department [department]
+  (into [] (sql/query connection ["select * from employees where department = ?" department])))
+
+(defn insert-employee [name salary department]
+  (sql/insert! connection :employees [:name :salary :department] [name salary (set-department department)]))
+
+ (defn delete-employee [id]
+ (sql/delete! connection :employees
+            ["id = ?" id]))
+ 
+ 
+ (defn md5 [^String s]
+  (let [algorithm (MessageDigest/getInstance "MD5")
+        raw (.digest algorithm (.getBytes s))]
+    (format "%032x" (BigInteger. 1 raw))))
+
+;; admin
+
+ (defn login [username password]
+   (into [] (sql/query connection ["select * from user where username = ? and password=?" username (md5 password)])))
